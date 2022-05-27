f35ec69647d56734a3b8aab5b9bd81552864ed77
Aizat <aayzat2004@gmail.com>
2022-05-27 12:44:24 +0300

    2.0

--- Задание ОП.py
+++ Задание ОП.py
@@ -2,7 +2,7 @@
 
 def encryption():
     alfavit_EU = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    for i in range(10):
+    for i in range(10): # 10 попыток првильно ввести правильно число
         away=int(input("Расшифрование (1), Шифрование(2) : "))
         if away!=1 and away!=2:
             print('Неправильные данные , выберите (1) или (2) ')
@@ -45,7 +45,7 @@ def encryption():
                     continue
             if len(message_2) % block !=0:
                 for i in range(block - len(message_2) % block) :
-                    message_2=message_2+"/"
+                    message_2=message_2+"\0"
             itog_2=""
             for i in range (len(message_2)//block):
                 for j in range(block):
@@ -54,7 +54,7 @@ def encryption():
             print(itog_2)
         if way==3:
             block_3=int(input("Введите количество слов в одном блоке: "))
-            for i in range(10):
+            for i in range(10):# 10 попыток првильно ввести правильно число (ключ)
                 smeshenie_3 = input("Введите ключ щифрования:  ")
                 if len(smeshenie_3) != block_3:
                     print("Вы ввели неправильный ключ , попробуйте заново ... ")
@@ -78,7 +78,7 @@ def encryption():
 
             if len(sms_3) % block_3 !=0:
                 for i in range(block_3 - len(sms_3) % block_3) :
-                    message_3=message_3+" / "
+                    message_3=message_3+" \0"
             sms_3 = str.split(message_3)
             itog_3=""
             for i in range(len(sms_3) // block_3):
@@ -103,7 +103,7 @@ def encryption():
             print(itog_1.lower())
         elif way==2:
             block=int(input("Введите количество элементов в одном блоке : "))
-            for l in range (10):
+            for l in range (10):# 10 попыток првильно ввести правильно число ( ключ расшифровки)
                 smeshenie_2 = input('Ключ расшифровки: ')
                 if len(smeshenie_2)!= block:
                     print("Вы ввели неправильный ключ , попробуйте заново ... ")
@@ -121,15 +121,18 @@ def encryption():
                     break
                 else:
                     continue
-            smm=smeshenie_2[::-1]
-            if len(message_2) % block !=0:
-                for i in range(block - len(message_2) % block) :
-                    message_2=message_2+"/"
+
             itog_2=""
+            r=""# вспомогательные элементы для создания ключа расшифровки ( обратного ключа шифрования)
+            reverse=[0]*block
+            for i in range(block):
+                r=r + str(i)
+            for i in range(block):
+                reverse[int(smeshenie_2[i])]=r[i]
             for i in range (len(message_2)//block):
                 for j in range(block):
-                    itog_2=itog_2 +message_2[i*block + int(smeshenie_2[j])]
-            s=itog_2.replace("/","")
+                    itog_2=itog_2 +message_2[i*block + int(reverse[j])]
+            s=itog_2.replace("\0","")
             print("Сообщение после шифрования: ")
             print(s)
         if way==3:
@@ -154,16 +157,26 @@ def encryption():
                     break
                 else:
                     continue
-            sms_3 = str.split(message_3)
-            if len(sms_3) % block_3 !=0:
-                for i in range(block_3 - len(sms_3) % block_3) :
-                    message_3=message_3+" / "
+            for i in range(2,len(message_3)):
+                if  message_3[i]==" " and message_3[i-1]==" " and message_3[i-2]==" ":
+                    message_3 = message_3[:i-1]+"/"+message_3[i:]
+
+            sms_3 = str.split(message_3)  # Разделение строки на массив где элементы слова
+
+            if len(sms_3) % block_3 !=0:# Дополнение до длины такой , чтобы нацело делилось на количетсво
+                message_3 = message_3+" /"
             sms_3 = str.split(message_3)
             itog_3=""
-            for i in range(len(sms_3) // block_3):
+            r=""# вспомогательные элементы для создания ключа расшифровки ( обратного ключа шифрования)
+            reverse=[0]*block_3
+            for i in range(block_3):
+                r=r + str(i)
+            for i in range(block_3):
+                reverse[int(smeshenie_3[i])]=r[i]
+            for i in range(len(sms_3) // block_3):# само "расшифрование"
                 for j in range(block_3):
-                    itog_3 = itog_3 + " "+sms_3[i * block_3 + int(smeshenie_3[j])]
-            s=itog_3.replace("/"," ")
-            print("Сообщение после шифрования: ")
+                    itog_3 = itog_3 + " "+sms_3[i * block_3 + int(reverse[j])]
+            s=itog_3.replace("/"," ") # тут все понятно
+            print("Сообщение после расшифрования: ")
             print(s)
 encryption()
