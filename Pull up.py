d44040a7e9662d56c7041a3ff81030d4614e65b7
Aizat <aayzat2004@gmail.com>
2022-09-01 20:51:59 +0300

    version 1

--- Pull up.py
+++ Pull up.py
@@ -1,11 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+import math
 
 x = [66,66+5,66+10,66+15,66+19,66+24,66+34,66+38]
 y = [23,20,17,11,9,7,4,2]
 x1=[66,66+38]
 y1=[23,2]
+
+
 fig, ax = plt.subplots()
 ax.xaxis.set_major_locator(ticker.MultipleLocator(3))
 
